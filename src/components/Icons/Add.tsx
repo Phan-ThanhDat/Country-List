c5e0fe8d6d90c54119a920d70fd9364d70e8f6f4
hongduc-phan <phduc.phanhongduc@gmail.com>
2020-05-23 11:55:53 +0300

    Icons components

---
+++ src/components/Icons/Add.tsx
@@ -0,0 +1,18 @@
+import React, { SVGAttributes } from 'react'
+
+const AddIcon: React.FC = (props: SVGAttributes<HTMLOrSVGElement>) => {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+      <g fill="none" fillRule="evenodd">
+        <path
+          fill="currentColor"
+          fillRule="nonzero"
+          d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z"
+        />
+        <path d="M0 0h24v24H0z" />
+      </g>
+    </svg>
+  )
+}
+
+export default AddIcon
