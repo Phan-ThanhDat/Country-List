9e29b7b29502f1a9a20566af3d36402e4999e852
hongduc-phan <phduc.phanhongduc@gmail.com>
2020-05-23 11:25:50 +0300

    move Typo to components folder and making Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import Routes from './Routes'
+import Button from './components/Button'
 
 export default function App() {
   return (
     <>
+      <Button variant="icon">Secondary</Button>
       <Routes />
     </>
   )
