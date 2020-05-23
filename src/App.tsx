911b10593464a4a4c0dd2b147c7fca68b1b26f2b
hongduc-phan <phduc.phanhongduc@gmail.com>
2020-05-23 12:50:20 +0300

    Loading component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import Routes from './Routes'
 import Button, { ButtonVariants } from './components/Button'
+import Loading from './components/Loading/Loading'
 
 export default function App() {
   return (
     <>
       <Button variant={ButtonVariants.Secondary}>Secondary</Button>
+      <Loading />
       <Routes />
     </>
   )
