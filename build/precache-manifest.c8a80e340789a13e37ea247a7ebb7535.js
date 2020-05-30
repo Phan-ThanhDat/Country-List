f008ad31bdceaa8de2258c875f628acbf6845766
hongduc-phan <phduc.phanhongduc@gmail.com>
2020-05-31 01:16:16 +0300

    build

---
+++ build/precache-manifest.c8a80e340789a13e37ea247a7ebb7535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2ec8c741847afd9f6efbe2c9033202",
+    "url": "/full-stack-4-typescript-redux-template/index.html"
+  },
+  {
+    "revision": "206a3ea38a35c3a2b9b4",
+    "url": "/full-stack-4-typescript-redux-template/static/css/main.51dd085d.chunk.css"
+  },
+  {
+    "revision": "a0c0679716c3bb53cb46",
+    "url": "/full-stack-4-typescript-redux-template/static/js/2.8ba9c451.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/full-stack-4-typescript-redux-template/static/js/2.8ba9c451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "206a3ea38a35c3a2b9b4",
+    "url": "/full-stack-4-typescript-redux-template/static/js/main.aaca2073.chunk.js"
+  },
+  {
+    "revision": "55af24e1f18c1fcb80fe",
+    "url": "/full-stack-4-typescript-redux-template/static/js/runtime-main.c0271653.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f4be8ba97fb5ee5b013e13842beefd80.js"
+  "/full-stack-4-typescript-redux-template/precache-manifest.c8a80e340789a13e37ea247a7ebb7535.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/full-stack-4-typescript-redux-template/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
