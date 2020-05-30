75ba49bc6edd2d0d79d096992dfe2b88b60b1a9b
hongduc-phan <phduc.phanhongduc@gmail.com>
2020-05-31 01:01:19 +0300

    build project

---
+++ build/precache-manifest.f4be8ba97fb5ee5b013e13842beefd80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e9f8f10d5d6198631c32d8adffbafc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "701efb86ee5ff3120f24",
+    "url": "/static/css/main.51dd085d.chunk.css"
+  },
+  {
+    "revision": "a0c0679716c3bb53cb46",
+    "url": "/static/js/2.8ba9c451.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8ba9c451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "701efb86ee5ff3120f24",
+    "url": "/static/js/main.d8a1d4d5.chunk.js"
+  },
+  {
+    "revision": "247b2672cef748d90091",
+    "url": "/static/js/runtime-main.17f0c553.js"
+  }
+]);
