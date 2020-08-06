0516e16d4d5c61c480902d65acb593d3a5141930
hongduc-phan <phduc.phanhongduc@gmail.com>
2020-08-06 11:40:23 +0300

    Lazy load for Coutry list

---
+++ build/precache-manifest.514bc3b1cae7c550151fb21e2680000c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970ba2db08e8663726dabd37d5028ad0",
+    "url": "/full-stack-4-typescript-redux-template/index.html"
+  },
+  {
+    "revision": "0a536fddc9d0cb227be4",
+    "url": "/full-stack-4-typescript-redux-template/static/css/main.51dd085d.chunk.css"
+  },
+  {
+    "revision": "d3ab1cb7cbd685ae6d6b",
+    "url": "/full-stack-4-typescript-redux-template/static/js/2.d9058287.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/full-stack-4-typescript-redux-template/static/js/2.d9058287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a536fddc9d0cb227be4",
+    "url": "/full-stack-4-typescript-redux-template/static/js/main.c96e6feb.chunk.js"
+  },
+  {
+    "revision": "55af24e1f18c1fcb80fe",
+    "url": "/full-stack-4-typescript-redux-template/static/js/runtime-main.c0271653.js"
+  }
+]);
--- src/pages/Countries/Countries.tsx
+++ src/pages/Countries/Countries.tsx
@@ -4,6 +4,9 @@ import useStyles from './Countries.module'
 import Card from '../../components/Card'
 import { Countries as CountryType } from '../../types'
 
+import LazyLoad from 'react-lazyload'
+import Loading from '../../components/Loading'
+
 export interface ICountriesProps {
   list: CountryType[]
   searchTerm: string
@@ -15,15 +18,22 @@ const Countries: React.FC<ICountriesProps> = ({ list }: ICountriesProps) => {
   const renderCountries =
     list.length > 0 &&
     list.map((c: CountryType) => (
-      <Country
-        hasAddBtn={true}
-        country={c}
-        key={c.name}
-        name={c.name}
-        flag={c.flag}
-        region={c.region}
-        languague={c.languages}
-      />
+      <LazyLoad
+        key={c!.name}
+        height={10}
+        offset={[-10, 10]}
+        placeholder={<Loading />}
+      >
+        <Country
+          hasAddBtn={true}
+          country={c}
+          key={c.name}
+          name={c.name}
+          flag={c.flag}
+          region={c.region}
+          languague={c.languages}
+        />
+      </LazyLoad>
     ))
   return <Card className={classes.root}>{renderCountries}</Card>
 }
